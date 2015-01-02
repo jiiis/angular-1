c1eeed91e63c6cbca5158fadbddc40715f2b548e
Hao Chang <yngwie.chang@gmail.com>
2015-01-02 21:28:27 +1100

    Add filters and images

--- web/js/app.js
+++ web/js/app.js
@@ -10,6 +10,10 @@
             name: 'erNiuNiu',
             price: 2.95,
             description: 'This is my lovely wife, erNiuNiu.',
+            image: {
+                full: 'images/gem_1.png',
+                thumb: 'images/gem_1_tb.png'
+            },
             canPurchase: true,
             soldOut: false
         },
@@ -17,6 +21,10 @@
             name: 'jibaGong',
             price: 1,
             description: 'This is me, and I\'m much cheaper than erNiuNiu.',
+            image: {
+                full: 'images/gem_2.png',
+                thumb: 'images/gem_2_tb.png'
+            },
             canPurchase: true,
             soldOut: false
         },
@@ -24,6 +32,10 @@
             name: 'pangCaiCai',
             price: 10.5,
             description: 'This is my precious pangCaiCai. We love her a lot!',
+            image: {
+                full: 'images/gem_3.png',
+                thumb: 'images/gem_3_tb.png'
+            },
             canPurchase: false,
             soldOut: false
         }
