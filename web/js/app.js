a1700d7ae1a1a4f9618f5601e03b20e15c0f5c6d
Hao Chang <yngwie.chang@gmail.com>
2015-01-03 18:10:50 +1100

    Add product gallery and form validation

--- web/js/app.js
+++ web/js/app.js
@@ -6,10 +6,24 @@
             name: 'erNiuNiu',
             price: 2.95,
             description: 'This is my lovely wife, erNiuNiu.',
-            image: {
-                full: 'images/gem_1.png',
-                thumb: 'images/gem_1_tb.png'
+            icon: {
+                full: 'images/icon_1.png',
+                thumb: 'images/icon_1_tb.png'
             },
+            images: [
+                {
+                    full: 'images/gem_1.gif',
+                    thumb: 'images/gem_1_tb.png'
+                },
+                {
+                    full: 'images/gem_2.gif',
+                    thumb: 'images/gem_2_tb.png'
+                },
+                {
+                    full: 'images/gem_3.gif',
+                    thumb: 'images/gem_3_tb.png'
+                }
+            ],
             reviews: [
                 {
                     stars: 5,
@@ -29,10 +43,25 @@
             name: 'jibaGong',
             price: 1,
             description: 'This is me, and I\'m much cheaper than erNiuNiu.',
-            image: {
-                full: 'images/gem_2.png',
-                thumb: 'images/gem_2_tb.png'
+            icon: {
+                full: 'images/icon_2.png',
+                thumb: 'images/icon_2_tb.png'
             },
+            images: [
+                {
+                    full: 'images/gem_4.gif',
+                    thumb: 'images/gem_4_tb.png'
+                },
+                {
+                    full: 'images/gem_5.gif',
+                    thumb: 'images/gem_5_tb.png'
+                },
+                {
+                    full: 'images/gem_6.gif',
+                    thumb: 'images/gem_6_tb.png'
+                }
+            ],
+            reviews: [],
             canPurchase: true,
             soldOut: false
         },
@@ -40,10 +69,25 @@
             name: 'pangCaiCai',
             price: 10.5,
             description: 'This is my precious pangCaiCai. We love her a lot!',
-            image: {
-                full: 'images/gem_3.png',
-                thumb: 'images/gem_3_tb.png'
+            icon: {
+                full: 'images/icon_3.png',
+                thumb: 'images/icon_3_tb.png'
             },
+            images: [
+                {
+                    full: 'images/gem_7.gif',
+                    thumb: 'images/gem_7_tb.png'
+                },
+                {
+                    full: 'images/gem_8.gif',
+                    thumb: 'images/gem_8_tb.png'
+                },
+                {
+                    full: 'images/gem_9.gif',
+                    thumb: 'images/gem_9_tb.png'
+                }
+            ],
+            reviews: [],
             canPurchase: false,
             soldOut: false
         }
@@ -62,6 +106,31 @@
 
         this.checkTab = function(tab) {
             return this.tab === tab;
+        };
+    });
+
+    app.controller('GalleryController', function() {
+        this.current = 0;
+
+        this.setImage = function(index) {
+            this.current = index;
+        };
+
+        this.checkImage = function(index) {
+            return this.current === index;
+        };
+    });
+
+    app.controller('ReviewController', function() {
+        this.review = {};
+
+        this.submitReview = function(product) {
+            if (typeof product.reviews === 'undefined') {
+                product.reviews = [];
+            }
+
+            product.reviews.push(this.review);
+            this.review = {};
         };
     });
 })();
